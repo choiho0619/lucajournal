a984d087c53be368d352e8b052e0fdd8c3c8c5a1
choiho0619 <choiho0619@gmail.com>
2026-07-21 21:59:57 +0900

    굿즈샵(링크형 갤러리) 신설 및 nav 10개 파일에 메뉴 반영
    - shop.html 신규: category.html 템플릿(head/nav/footer/page-main) 재사용, 카드형 상품 갤러리(전체/실물 상품/디지털 다운로드 필터)
    - shop-products.js: 상품 데이터 하드코딩 배열(실물 1 + 디지털 1 placeholder), 스마트스토어 외부 링크(target=_blank rel=noopener noreferrer)
    - 10개 html 전체에 "굿즈샵" nav 링크 추가(데스크톱/모바일 햄버거 공용 nav-links 1곳 수정으로 양쪽 반영)
    - DB/백엔드 변경 없음, PG 연동 없음

---
+++ shop-products.js
@@ -0,0 +1,19 @@
+export const PRODUCTS = [
+  {
+    id: "psalm23-print",
+    name: "시편 23편 캘리그라피 액자용 프린트",
+    type: "physical", // "physical" | "digital"
+    price: "12,000원",
+    image: "/assets/shop/psalm23-print.jpg",
+    storeUrl: "https://smartstore.naver.com/[스토어명]/products/[상품번호]",
+  },
+  {
+    id: "psalm23-digital",
+    name: "시편 23편 캘리그라피 디지털 파일",
+    type: "digital",
+    price: "3,000원",
+    image: "/assets/shop/psalm23-digital.jpg",
+    storeUrl: "https://smartstore.naver.com/[스토어명]/products/[상품번호]",
+  },
+  // 실제 상품은 이 배열에 계속 추가
+];
